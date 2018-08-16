59e7a0365e915569d1b8208794173392d48cf7af
Vicky-w <595288507@qq.com>
2018-08-16 17:22:13 +0800

    create log_process demo

--- log_monitoring_demo/src/github.com/Vicky-w/log_monitoring_demo/log_process.go
+++ log_monitoring_demo/src/github.com/Vicky-w/log_monitoring_demo/log_process.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/influxdata/influxdb/client/v2"
 	"io"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"regexp"
@@ -16,18 +20,22 @@ import (
 type Reader interface {
 	Read(rc chan []byte)
 }
+
 type Writer interface {
 	Write(wc chan *Message)
 }
+
 type LogProcess struct {
 	rc    chan []byte
 	wc    chan *Message
 	read  Reader
 	write Writer
 }
+
 type ReadFromFile struct {
-	path string //读取文件路径
+	path string // 读取文件的路径
 }
+
 type WriteToInfluxDB struct {
 	influxDBDsn string // influx data source
 }
@@ -39,29 +47,99 @@ type Message struct {
 	UpstreamTime, RequestTime    float64
 }
 
+// 系统状态监控
+type SystemInfo struct {
+	HandleLine   int     `json:"handleLine"`   // 总处理日志行数
+	Tps          float64 `json:"tps"`          // 系统吞出量
+	ReadChanLen  int     `json:"readChanLen"`  // read channel 长度
+	WriteChanLen int     `json:"writeChanLen"` // write channel 长度
+	RunTime      string  `json:"runTime"`      // 运行总时间
+	ErrNum       int     `json:"errNum"`       // 错误数
+}
+
+const (
+	TypeHandleLine = 0
+	TypeErrNum     = 1
+)
+
+var TypeMonitorChan = make(chan int, 200)
+
+type Monitor struct {
+	startTime time.Time
+	data      SystemInfo
+	tpsSli    []int
+}
+
+func (m *Monitor) start(lp *LogProcess) {
+
+	go func() {
+		for n := range TypeMonitorChan {
+			switch n {
+			case TypeErrNum:
+				m.data.ErrNum += 1
+			case TypeHandleLine:
+				m.data.HandleLine += 1
+			}
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second * 5)
+	go func() {
+		for {
+			<-ticker.C
+			m.tpsSli = append(m.tpsSli, m.data.HandleLine)
+			if len(m.tpsSli) > 2 {
+				m.tpsSli = m.tpsSli[1:]
+			}
+		}
+	}()
+
+	http.HandleFunc("/monitor", func(writer http.ResponseWriter, request *http.Request) {
+		m.data.RunTime = time.Now().Sub(m.startTime).String()
+		m.data.ReadChanLen = len(lp.rc)
+		m.data.WriteChanLen = len(lp.wc)
+
+		if len(m.tpsSli) >= 2 {
+			m.data.Tps = float64(m.tpsSli[1]-m.tpsSli[0]) / 5
+		}
+
+		ret, _ := json.MarshalIndent(m.data, "", "\t")
+		io.WriteString(writer, string(ret))
+	})
+
+	http.ListenAndServe(":9193", nil)
+}
+
 func (r *ReadFromFile) Read(rc chan []byte) {
-	//读取文件
+
+	// 打开文件
 	f, err := os.Open(r.path)
 	if err != nil {
-		panic(fmt.Sprintf("open file error : %s", err.Error()))
+		panic(fmt.Sprintf("open file error:%s", err.Error()))
 	}
 	//从文件末尾开始逐行读取文件
 	f.Seek(0, 2)
 	rd := bufio.NewReader(f)
+
 	for {
 		line, err := rd.ReadBytes('\n')
-		if err == io.EOF { //日志产生  结尾判断
+		if err == io.EOF {
 			time.Sleep(500 * time.Millisecond)
 			continue
 		} else if err != nil {
-			panic(fmt.Sprintf("ReadBytes errors : %s", err.Error()))
+			panic(fmt.Sprintf("ReadBytes error:%s", err.Error()))
 		}
+		TypeMonitorChan <- TypeHandleLine
 		//rc <- line
 		//去除换行符
 		rc <- line[:len(line)-1]
 	}
+
 }
 func (w *WriteToInfluxDB) Write(wc chan *Message) {
+	//初始化数据
+	infSli := strings.Split(w.influxDBDsn, "@")
+
 	//初始化influxdb client
 	// 从Write Channel中读取监控数据
 	//构造数据并写入influxdb
@@ -72,8 +150,52 @@ func (w *WriteToInfluxDB) Write(wc chan *Message) {
 		内置http支持， 使用http读写
 		类sql的灵活查询（max、min、sum 等）
 	*/
+	// Create a new HTTPClient
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr:     infSli[0],
+		Username: infSli[1],
+		Password: infSli[2],
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 	for v := range wc {
-		fmt.Println(v)
+		// Create a new point batch
+		bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+			Database:  infSli[3],
+			Precision: infSli[4],
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create a point and add to batch
+		// Tags: Path, Method, Scheme, Status
+		tags := map[string]string{"Path": v.Path, "Method": v.Method, "Scheme": v.Scheme, "Status": v.Status}
+		// Fields: UpstreamTime, RequestTime, BytesSent
+		fields := map[string]interface{}{
+			"UpstreamTime": v.UpstreamTime,
+			"RequestTime":  v.RequestTime,
+			"BytesSent":    v.BytesSent,
+		}
+
+		pt, err := client.NewPoint("nginx_log", tags, fields, v.TimeLocal)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bp.AddPoint(pt)
+
+		// Write the batch
+		if err := c.Write(bp); err != nil {
+			log.Fatal(err)
+		}
+
+		// Close client resources
+		//if err := c.Close(); err != nil {
+		//	log.Fatal(err)
+		//}
+		log.Println(v)
+		log.Println("write success !")
 	}
 }
 
@@ -82,55 +204,81 @@ func (l *LogProcess) Process() {
 	//正则提取所需的监控数据（path status method 等）
 	//写入Write Channel
 	/**
-	172.0.0.12 - -[04/Mar/2018:13:49:52 +0000] http "GET /vickywang?query=t HTTP/1.0" 200 2133 "-"
-	"KeepAliveClient" "-" 1.005 1.854
-
-	([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([~\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)
+	172.0.0.12 - - [04/Mar/2018:13:49:52 +0000] http "GET /vickywang?query=t HTTP/1.0" 200 2133 "-" "KeepAliveClient" "-" 1.005 1.854
 	*/
-	r := regexp.MustCompile(`([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([~\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)`)
+	r := regexp.MustCompile(`([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([^\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)`)
 	loc, _ := time.LoadLocation("Asia/Shanghai")
 	for v := range l.rc {
+
 		ret := r.FindStringSubmatch(string(v))
 		if len(ret) != 14 {
+			TypeMonitorChan <- TypeErrNum
 			log.Println("FindStringSubmatch fail:", string(v))
 			continue
 		}
+
 		message := &Message{}
-		t, err := time.ParseInLocation("02/Jan/2006:15:04:05 +0000", ret[4], loc) //6 1 2 3 4 5
+		t, err := time.ParseInLocation("02/Jan/2006:15:04:05 +0000", ret[4], loc)
 		if err != nil {
-			log.Println("ParseInLocation fail:", ret[4])
+			TypeMonitorChan <- TypeErrNum
+			log.Println("ParseInLocation fail:", err.Error(), ret[4])
+			continue
 		}
 		message.TimeLocal = t
+
 		byteSent, _ := strconv.Atoi(ret[8])
 		message.BytesSent = byteSent
+
 		//GET /vickywang?query=t HTTP/1.0
+
 		reqSli := strings.Split(ret[6], " ")
 		if len(reqSli) != 3 {
+			TypeMonitorChan <- TypeErrNum
 			log.Println("strings.Split fail", ret[6])
 			continue
 		}
-		message.Method = ret[0]
-		u, err := url.Parse(ret[1])
+		message.Method = reqSli[0]
+
+		u, err := url.Parse(reqSli[1])
 		if err != nil {
 			log.Println("url parse fail:", err)
+			TypeMonitorChan <- TypeErrNum
+			continue
 		}
 		message.Path = u.Path
+
 		message.Scheme = ret[5]
 		message.Status = ret[7]
+
 		upstreamTime, _ := strconv.ParseFloat(ret[12], 64)
 		requestTime, _ := strconv.ParseFloat(ret[13], 64)
 		message.UpstreamTime = upstreamTime
 		message.RequestTime = requestTime
+
+		log.Println(message)
+		//log.Println(message.TimeLocal)
+		//log.Println(message.BytesSent)
+		//log.Println(message.Method)
+		//log.Println(message.Path)
+		//log.Println(message.Scheme)
+		//log.Println(message.Status)
+		//log.Println(message.UpstreamTime)
+		//log.Println(message.RequestTime)
 		l.wc <- message
 	}
 }
 
 func main() {
+	var path, influxDsn string
+	flag.StringVar(&path, "path", "./tmp/access.log", "read file path")
+	flag.StringVar(&influxDsn, "influxDsn", "http://172.16.17.137:8086@vickywang@vickywangpass@mydb@s", "influx data source")
+	flag.Parse()
+
 	r := &ReadFromFile{
-		path: "./tmp/access.log",
+		path: path,
 	}
 	w := &WriteToInfluxDB{
-		influxDBDsn: "username&password",
+		influxDBDsn: influxDsn, //用户名 密码 数据库 精度
 	}
 	lp := &LogProcess{
 		rc:    make(chan []byte),
@@ -141,12 +289,22 @@ func main() {
 	go lp.read.Read(lp.rc)
 	go lp.Process()
 	go lp.write.Write(lp.wc)
-	time.Sleep(30 * time.Second)
+	time.Sleep(300 * time.Second)
 }
 
 /*
-go run log_process.log
+go run log_process.log -path ./tmp/access.log -influxDsn http://172.16.17.137:8086@vickywang@vickywangpass@mydb@s
 
+echo 172.0.0.12 - - [15/Aug/2018:20:34:07 +0000] https \"GET /bar HTTP/1.0\" 200 1261 \"-\" \"KeepAliveClient\" \"-\" - 0.017 >> access.log
 echo VickyWang01 >> access.log
 echo VickyWang02 >> access.log
+
+监控需求：
+  某个协议下的某个请求在某个请求方法的 QPS&响应时间&流量
+
+Tags：Path, Method, Scheme, Status
+Fields： UpstreamTime, RequestTime ，BytesSent
+Time：TimeLocal
+
+curl -XPOST "http://localhost:8086/query" --data-urlencode "q=CREATE DATABASE mydb"
 */
